fa7e5ec4283869af250f347b627730c2f5f49a4b
HJU <hjuijterlinde@hotmail.com>
2019-06-26 15:08:52 +0200

    demo

--- src/main/java/com/Semafoor/ToDoList/ToDoListApplication.java
+++ src/main/java/com/Semafoor/ToDoList/ToDoListApplication.java
@@ -2,12 +2,20 @@ package com.Semafoor.ToDoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class ToDoListApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
-
 }
