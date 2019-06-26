3ff79d799299d4d266e981c4b368ff046b8b9ccf
HJU <hjuijterlinde@hotmail.com>
2019-06-26 15:57:41 +0200

    db config update

--- src/main/java/com/Semafoor/ToDoList/config/DataBaseConfig.java
+++ src/main/java/com/Semafoor/ToDoList/config/DataBaseConfig.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataBaseConfig {
 
-    @Value("${spring.datasource.url}")
+    @Value("postgres://hpdvjhhcethmcx:95815ced1cd592dfa8710de657d55bf187752abbb8d609c45557e561aebd7758@ec2-174-129-41-127.compute-1.amazonaws.com:5432/d66h65o252cps1")
     private String dbUrl;
 
     @Bean
